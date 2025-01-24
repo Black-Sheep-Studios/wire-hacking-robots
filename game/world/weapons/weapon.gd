060197a40ddc4455258d6d252dd00c49b7a6c03e
Tim Heuett <tim.heuett@gmail.com>
2025-01-23 19:23:00 -0800

    implement weapon delay

--- game/world/weapons/weapon.gd
+++ game/world/weapons/weapon.gd
@@ -12,8 +12,13 @@ enum DamageType {
 
 @export var stats: WeaponStats
 
+@onready var _delay_timer: Timer = _init_delay_timer()
+var cooldown: bool = false
+
 
 func fire(direction: Vector2) -> void:
+	if cooldown: return
+
 	var bullet = stats.bullet_scene.instantiate()
 
 	var initial_velocity = direction * stats.bullet_speed
@@ -21,3 +26,16 @@ func fire(direction: Vector2) -> void:
 	bullet.init(initial_position, initial_velocity, shooter, stats)
 
 	container.add_child(bullet)
+	cooldown = true
+	_delay_timer.start()
+
+
+func _init_delay_timer() -> Timer:
+	_delay_timer = Timer.new()
+	_delay_timer.set_wait_time(stats.fire_delay_seconds)
+	_delay_timer.set_one_shot(true)
+	_delay_timer.timeout.connect(func() -> void: 
+		cooldown = false
+	)
+	add_child(_delay_timer)
+	return _delay_timer
